c206d34d75c42622dea5a2590f4722191c5e91dd
Andrew Chang <changdrew@berkeley.edu>
2015-10-15 22:01:12 -0700

    donezos

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -2,4 +2,12 @@ class PokemonsController < ApplicationController
   def index
     @pokemons = Pokemon.all
   end
+
+  def edit
+    @pokemons = Pokemon.find(params[:id])
+  end
+
+  def pokemon_params
+    params.require(:pokemon).permit(:name, :description, :level, :user_id)
+  end
 end
